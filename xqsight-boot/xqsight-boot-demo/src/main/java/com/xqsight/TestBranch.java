0a6d602d74303ab1e8cedbcabc9f6e1e0c50644e
wangganggang <wggorking@163.com>
2017-06-26 09:54:21 +0800

    branch develop test

---
+++ xqsight-boot/xqsight-boot-demo/src/main/java/com/xqsight/TestBranch.java
@@ -0,0 +1,10 @@
+package com.xqsight;
+
+/**
+ * Created by Administrator on 2017/6/26.
+ */
+public class TestBranch {
+    public static void mian(String argss[]){
+        System.out.println("test branch develop");
+    }
+}
